4101a52b7fc4a49f6c9eb848e9b51d0e062191d6
Maximilian Maria Horzela <maximilian.horzela@kit.edu>
2020-10-06 12:10:28 +0200

    Rearranged setup scripts for remote and local tasks

--- setup/setup_herwig.sh
+++ setup/setup_herwig.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
-cd /local/scratch/ssd/mhorzela
+# source grid environment
+source /cvmfs/grid.cern.ch/centos7-wn-4.0.5-1_umd4v1/etc/profile.d/setup-c7-wn-example.sh
 
-
+# setup Herwig
 export INSTALL_LOC=/cvmfs/pheno.egi.eu/Herwig/Herwig-7-2
 export HERWIGPATH=/cvmfs/pheno.egi.eu/Herwig/Herwig-7-2
 export PATH=$INSTALL_LOC/bin:$PATH
@@ -10,8 +11,6 @@ export LHAPDF=/cvmfs/pheno.egi.eu/Herwig
 export PATH=$PATH:$LHAPDF/bin
 export LD_LIBRARY_PATH=$LHAPDF/lib:$LD_LIBRARY_PATH
 export PYTHONPATH=$LHAPDF/lib/python2.7/site-packages:$PYTHONPATH
-
+# activate Herwig
 source /cvmfs/pheno.egi.eu/Herwig/bin/activate
 
-Herwig read inputfiles/LHC-ZJetMerging.in
-Herwig run LHC-Z-Merging.run -N 1000 -s 12345
