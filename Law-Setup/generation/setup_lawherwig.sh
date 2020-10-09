e2e49be6d693074ebe740700d75717af39e3b83a
Maximilian Maria Horzela <maximilian.horzela@kit.edu>
2020-10-09 10:14:50 +0200

    Fixing enum version in bootstrap script

--- Law-Setup/generation/setup_lawherwig.sh
+++ Law-Setup/generation/setup_lawherwig.sh
@@ -19,7 +19,7 @@ action(){
     export ANALYSIS_DATA_PATH="$ANALYSIS_PATH"
 
     export PATH="$PWD/law/bin:$PWD/luigi/bin:$PATH"
-    export PYTHONPATH="$PWD/enum34-1.1.6:$PWD/law:$PWD/luigi:$PWD/six:$PWD:$PYTHONPATH"
+    export PYTHONPATH="$PWD/enum34-1.1.10:$PWD/law:$PWD/luigi:$PWD/six:$PWD:$PYTHONPATH"
 
     # setup Herwig
     export INSTALL_LOC=/cvmfs/pheno.egi.eu/Herwig/Herwig-7-2
