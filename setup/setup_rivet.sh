91abee54edc9c8b33601e5b2893a6ea17323341c
Maximilian Maria Horzela <maximilian.horzela@kit.edu>
2020-11-05 17:04:00 +0100

    [Rivet] Setup scripts for remote and local execution

---
+++ setup/setup_rivet.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# source grid environment
+source /cvmfs/grid.cern.ch/centos7-wn-4.0.5-1_umd4v1/etc/profile.d/setup-c7-wn-example.sh
+
+# Set up Rivet
+# Set up GCC and Python (v3 also supported):
+source /cvmfs/sft.cern.ch/lcg/releases/LCG_96/Python/2.7.16/x86_64-centos7-gcc62-opt/Python-env.sh
+# Set up Rivet
+source /cvmfs/sft.cern.ch/lcg/releases/LCG_96b/MCGenerators/rivet/3.1.2/x86_64-centos7-gcc8-opt/rivetenv-genser.sh
+# Set up a working LaTeX (needed if on lxplus7)
+export PATH=/cvmfs/sft.cern.ch/lcg/external/texlive/2016/bin/x86_64-linux:$PATH
+
+# Export rivet analysis path for plugin analyses
+export RIVET_ANALYSIS_PATH="$ANALYSIS_PATH/generation/analyses:$RIVET_ANALYSIS_PATH"
+#export RIVET_INFO_PATH="$RIVET_ANALYSIS_PATH"
