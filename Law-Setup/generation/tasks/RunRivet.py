ef0cd711899771ad8d250317aab51ef53946f3e5
Maximilian Maria Horzela <maximilian.horzela@kit.edu>
2020-11-23 14:57:27 +0100

    [RunRivet] Dirty working version for Rivet

--- Law-Setup/generation/tasks/RunRivet.py
+++ Law-Setup/generation/tasks/RunRivet.py
@@ -4,6 +4,7 @@ import law
 import luigi
 import os
 import random
+import glob
 
 from subprocess import PIPE
 from law.util import interruptable_popen
@@ -25,29 +26,56 @@ class RunRivet(Task, HTCondorWorkflow):
     rivet_analyses = luigi.ListParameter()
 
     def get_incr_filelist(self):
-
-        incr_filenumber_list = list(range(int(self.number_of_jobs))[::int(self.files_per_job)])
-
+        """
+        # Method to identify the HepMC inputfile splitting.
+        # It returns a list of file identifiers (int), which define the lower and upper borders 
+        # of the filelist for all branch tasks.
+        """
+        _files_per_job = int(self.files_per_job)
+        _number_of_jobs = int(self.number_of_jobs)
         # remaining number of files not matched to job
-        _remaining_files = int(self.number_of_jobs)%int(self.files_per_job)
-        # if some files are not matched to a job, add them
-        if _remaining_files != 0:
-            incr_filenumber_list.append(int(self.number_of_jobs))
+        # _remaining_files = _number_of_jobs % _files_per_job
+
+        incr_filenumber_list = list(range(_number_of_jobs)[::_files_per_job])
+        # add the total number of jobs as right border
+        incr_filenumber_list.append(int(self.number_of_jobs))
+
+        #print("Incremental file number list: {}".format(incr_filenumber_list))
         
         return incr_filenumber_list
 
 
     def workflow_requires(self):
         # analyzing requires successfully generated events
-        return HerwigRun.req(self)
+        return {
+            #"HerwigRun": HerwigRun.req(self, branches=range(self.get_incr_filelist()[self.branch],self.get_incr_filelist()[self.branch+1]))
+            "HerwigRun": HerwigRun.req(self,branch=self.branch)
+        }
 
     def create_branch_map(self):
-        # each analysis job is indexed by the according number of HEPMC files to analyze                
-        return dict(enumerate(self.get_incr_filelist()[:-1]))
+        # each analysis job is indexed by the according number of HEPMC files to analyze  
+        # print("Branch map: {}".format(dict(enumerate(self.get_incr_filelist()[:-1]))))              
+        # return {
+        #     i: fileident for i, fileident in enumerate(self.get_incr_filelist()[:-1])
+        # }
+
+        # one by one job to inputfile matching
+        return {
+            jobnum: intjobnum for jobnum, intjobnum in enumerate(range(int(self.number_of_jobs))) 
+        }
 
     def requires(self):
         # each branch task requires existent HEPMC files to analyze
-        return HerwigRun.req(self, branches=range(self.get_incr_filelist()[self.branch],self.get_incr_filelist()[self.branch+1]))
+        # print("Branches: {}".format(range(self.get_incr_filelist()[self.branch],self.get_incr_filelist()[self.branch+1])))
+        # print(dir(self))
+        # print("Requirements start/end: {}".format(HerwigRun.req(self, start_branch=self.get_incr_filelist()[self.branch], end_branch=self.get_incr_filelist()[self.branch+1]-1)))
+        # print("Requirements 1,2,5: {}".format(HerwigRun.req(self, branches="1,2,5")))
+        # return [
+        #     HerwigRun.req(self, branch=_branch) for _branch in range(self.get_incr_filelist()[self.branch], self.get_incr_filelist()[self.branch+1])
+        # ]
+        return {
+            "HerwigRun": HerwigRun.req(self,branch=self.branch)
+        }
 
     def output(self):
         # 
@@ -77,17 +105,22 @@ class RunRivet(Task, HTCondorWorkflow):
         my_env = os.environ
         
         # identify and get the HEPMC files for analyzing
-        inputfile_list = []
-        for branch, target in self.input()['HerwigRun']["collection"].targets.items():
-            inputfile_list.append(target.localize('r').path)
+        print("Inputs: {}".format(self.input()))
+        # for target in self.input()['HerwigRun']:
+        #     with target.localize('r') as input_file:
+        #         os.system('tar -xvjf {}'.format(input_file.path))
+        with self.input()['HerwigRun'].localize('r') as input_file:
+            os.system('tar -xvjf {}'.format(input_file.path))
+
+        input_files = glob.glob('*.hepmc')
 
         # run Rivet analysis
         _rivet_exec = ["rivet"]
         _rivet_args = [
-            "-a {RIVET_ANALYSES}".format(RIVET_ANALYSES=" -a ".join(_rivet_analyses)),
-            "-o {OUTPUT_NAME}".format(OUTPUT_NAME="Rivet.yoda"),
-            "{HEPMC_FILES}".format(HEPMC_FILES=" ".join(inputfile_list))
-        ]
+            "--analysis={RIVET_ANALYSIS}".format(RIVET_ANALYSIS=_rivet_analysis) for _rivet_analysis in _rivet_analyses
+        ] + [
+            "--histo-file={OUTPUT_NAME}".format(OUTPUT_NAME="Rivet.yoda")
+        ] + glob.glob('*.hepmc')
 
         print('Executable: {}'.format(" ".join(_rivet_exec + _rivet_args)))
 
