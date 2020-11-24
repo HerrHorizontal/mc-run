12dee6eec7e631a68800663469837533b9a72ddd
Maximilian Maria Horzela <maximilian.horzela@kit.edu>
2020-11-24 12:53:42 +0100

    [Rivet] Added Z+j analysis for Rivet 3

--- Law-Setup/generation/analyses/ZplusJet_3.cc
+++ Law-Setup/generation/analyses/ZplusJet_3.cc
@@ -1,10 +1,13 @@
 // -*- C++ -*-
 #include "Rivet/Analysis.hh"
 #include "Rivet/Projections/FinalState.hh"
-#include "Rivet/Projections/FastJets.hh"
-#include "Rivet/Projections/DressedLeptons.hh"
-#include "Rivet/Projections/MissingMomentum.hh"
+#include "Rivet/Projections/ChargedFinalState.hh"
 #include "Rivet/Projections/PromptFinalState.hh"
+#include "Rivet/Projections/VetoedFinalState.hh"
+#include "Rivet/Projections/DressedLeptons.hh"
+#include "Rivet/Projections/FastJets.hh"
+#include "Rivet/Projections/JetAlg.hh"
+#include "Rivet/Projections/MissingMomentum.hh"
 
 namespace Rivet {
 
@@ -28,12 +31,14 @@ namespace Rivet {
       // The basic final-state projection:
       // all final-state particles within
       // the given eta acceptance
-      const FinalState fs(Cuts::abseta < 4.9);
+      const FinalState fs(Cuts::abseta < 5. && Cuts::pT > 100*MeV);
+      const ChargedFinalState cfs(fs);
+
 
       // The final-state particles declared above are clustered using FastJet with
       // the anti-kT algorithm and a jet-radius parameter 0.4
-      // muons and neutrinos are excluded from the clustering
-      FastJets jetfs(fs, FastJets::ANTIKT, 0.4, JetAlg::Muons::NONE, JetAlg::Invisibles::NONE);
+      // neutrinos are excluded from the clustering
+      FastJets jetfs(fs, FastJets::ANTIKT, 0.4, JetAlg::Muons::ALL, JetAlg::Invisibles::NONE);
       declare(jetfs, "jets");
 
       // FinalState of prompt photons and bare muons and electrons in the event
@@ -42,22 +47,57 @@ namespace Rivet {
 
       // Dress the prompt bare leptons with prompt photons within dR < 0.1,
       // and apply some fiducial cuts on the dressed leptons
-      Cut lepton_cuts = Cuts::abseta < 2.5 && Cuts::pT > 20*GeV;
+      Cut lepton_cuts = Cuts::abseta < 2.4 && Cuts::pT > 25*GeV;
       DressedLeptons dressed_leps(photons, bare_leps, 0.1, lepton_cuts);
       declare(dressed_leps, "leptons");
 
       // Missing momentum
-      declare(MissingMomentum(fs), "MET");
+      /// Out of acceptance particles treat as invisible
+      VetoedFinalState fs_onlyinacc(fs, (Cuts::abspid == PID::MUON && Cuts::abseta > 2.4) || 
+                                    (Cuts::abspid == PID::PHOTON && Cuts::abseta > 3.0) || 
+                                    (Cuts::abspid == PID::ELECTRON && Cuts::abseta > 3.0));
+      declare(MissingMomentum(fs_onlyinacc), "MET");
 
       // Book histograms
       // specify custom binning
-      book(_h["XXXX"], "myh1", 20, 0.0, 100.0);
-      book(_h["YYYY"], "myh2", logspace(20, 1e-2, 1e3));
-      book(_h["ZZZZ"], "myh3", {0.0, 1.0, 2.0, 4.0, 8.0, 16.0});
-      // take binning from reference data using HEPData ID (digits in "d01-x01-y01" etc.)
-      book(_h["AAAA"], 1, 1, 1);
-      book(_p["BBBB"], 2, 1, 1);
-      book(_c["CCCC"], 3, 1, 1);
+      /// Book histograms with variable bin size
+      book(_h["NJets"], "NJets", 10, 0.5, 10.5);
+
+      vector<double> binedges_Ystar = {0.0, 0.5, 1.0, 1.5, 2.0};
+      vector<double> binedges_Yboost = {0.0, 0.5, 1.0, 1.5, 2.0};
+      
+      vector<double> binedges_ZPt;
+      vector<double> binedges_PhiStarEta;
+
+      for(auto _ystar: binedges_Ystar){
+        for(auto _yboost: binedges_Yboost){
+          if(_ystar + _yboost > 2.) continue;
+          // extreme bin
+          if(_ystar>=2.0 && _yboost<0.5){
+            binedges_ZPt = {25., 30., 40., 50., 70., 90., 110., 150., 250.};
+            binedges_PhiStarEta = {0.4, 0.6, 0.8, 1.0, 5.};
+          }
+          // central bins
+          else if((_ystar<0.5 && _yboost<2.) || (_ystar<1. && _yboost<1.5) || (_ystar<1.5 && _yboost<1.)){
+            binedges_ZPt = {25., 30., 35., 40., 50., 60., 70., 80., 90., 100., 110., 130., 150., 170., 190., 220., 250., 400., 1000.};
+            binedges_PhiStarEta = {0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.2, 1.5, 2., 3., 4., 5., 7., 10., 15., 20., 30., 50.};
+          }
+          // edge bins
+          else {
+            binedges_ZPt = {25., 30., 35., 40., 45., 50., 60., 70., 80., 90., 100., 110., 130., 150., 170., 190., 250., 1000.};
+            binedges_PhiStarEta = {0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.2, 1.5, 2., 3., 5., 10., 15., 50.};
+          }
+
+          string _hist_ZPt_ident = "ZPtYs"+to_string(_ystar)+"Yb"+to_string(_yboost);
+          string _hist_ZPt_name = _hist_ZPt_ident;
+          string _hist_PhiStarEta_ident = "PhiStarEtaYs"+to_string(_ystar)+"Yb"+to_string(_yboost);
+          string _hist_PhiStarEta_name = _hist_PhiStarEta_ident;
+          
+          book(_h[_hist_ZPt_ident], _hist_ZPt_name, binedges_ZPt);
+          book(_h[_hist_PhiStarEta_ident], _hist_PhiStarEta_name, binedges_PhiStarEta);
+
+        }
+      }
 
     }
 
@@ -67,26 +107,89 @@ namespace Rivet {
 
       // Retrieve dressed leptons, sorted by pT
       vector<DressedLepton> leptons = apply<DressedLeptons>(event, "leptons").dressedLeptons();
+      MSG_DEBUG("Lepton multiplicity = " << leptons.size());
 
       // Retrieve clustered jets, sorted by pT, with a minimum pT cut
-      Jets jets = apply<FastJets>(event, "jets").jetsByPt(Cuts::pT > 30*GeV);
+      Jets jets = apply<FastJets>(event, "jets").jetsByPt(Cuts::absrap < 2.4 && Cuts::pT > 10*GeV);
 
-      // Remove all jets within dR < 0.2 of a dressed lepton
-      idiscardIfAnyDeltaRLess(jets, leptons, 0.2);
+      // Remove all jets within dR < 0.3 of a dressed lepton
+      idiscardIfAnyDeltaRLess(jets, leptons, 0.3);
+      MSG_DEBUG("Jet multiplicity = " << jets.size());
 
-      // Select jets ghost-associated to B-hadrons with a certain fiducial selection
-      Jets bjets = filter_select(jets, [](const Jet& jet) {
-        return  jet.bTagged(Cuts::pT > 5*GeV && Cuts::abseta < 2.5);
-      });
+      // Require at least one hard jet
+      if (jets.empty()) vetoEvent;      
+      if (jets.at(0).pT() <= 20*GeV) vetoEvent;
 
-      // Veto event if there are no b-jets
-      if (bjets.empty())  vetoEvent;
+      // Require at least two opposite sign leptons compatible with Z-boson mass and keep the pair closest to Zboson mass
+      bool _bosoncandidateexists = false;
+      double _massdiff = 20*GeV;
+      DressedLepton _muon = leptons.at(0);
+      DressedLepton _antimuon = leptons.at(0);
 
-      // Apply a missing-momentum cut
-      if (apply<MissingMomentum>(event, "MET").missingPt() < 30*GeV)  vetoEvent;
+      for (unsigned int it = 1; it < leptons.size(); ++it) {
+        for (unsigned int jt = 0; jt < it; ++jt) {
+          double _candidatemass = (leptons.at(it).mom() + leptons.at(jt).mom()).mass();
+          if (leptons.at(it).pid() == -leptons.at(jt).pid() && abs(_candidatemass - 91.1876*GeV) < _massdiff) {
+            _bosoncandidateexists = true;
+            _massdiff = abs(_candidatemass - 91.1876*GeV);
+            if (leptons.at(it).pid() > 0) {
+              _muon = leptons.at(it);
+              _antimuon = leptons.at(jt);
+            }
+            else {
+              _muon = leptons.at(jt);
+              _antimuon = leptons.at(it);
+            }
+          }
+          else continue;
+        }
+      }
 
-      // Fill histogram with leading b-jet pT
-      _h["XXXX"]->fill(bjets[0].pT()/GeV);
+      if (!(_bosoncandidateexists)) vetoEvent;
+
+
+      // Fill jet related histograms
+      _h["NJets"] -> fill(jets.size());
+
+      // Fill triple differential histograms
+      const double rap_Z = (_muon.mom() + _antimuon.mom()).rap();
+      const double pT_Z = (_muon.mom() + _antimuon.mom()).pT()/GeV;
+
+      const double thetastar = acos(tanh((_antimuon.mom().eta() - _muon.mom().eta())/2));
+      const double phistareta = tan(HALFPI - (_antimuon.mom().phi() - _muon.mom().phi())/2)*sin(thetastar);
+
+      const double rap_Jet1 = jets.at(0).rap();  
+
+      const double rap_star = 0.5 * abs(rap_Z - rap_Jet1);
+      const double rap_boost = 0.5 * abs(rap_Z + rap_Jet1);
+
+      /// Fill signal histograms
+      vector<double> binedges_Ystar = {0.5, 1.0, 1.5, 2.0, 2.5};
+      vector<double> binedges_Yboost = {0.5, 1.0, 1.5, 2.0, 2.5};
+
+      for(auto _ystar: binedges_Ystar){
+        for(auto _yboost: binedges_Yboost){
+          if(_ystar + _yboost > 3.) continue;
+          if((rap_star < _ystar) && (rap_boost < _yboost)){
+
+            // The histograms are named with the left bin border
+            _ystar -= 0.5;
+            _yboost -= 0.5;
+
+            string _hist_ZPt_ident = "ZPtYs"+to_string(_ystar)+"Yb"+to_string(_yboost);
+            string _hist_PhiStarEta_ident = "PhiStarEtaYs"+to_string(_ystar)+"Yb"+to_string(_yboost);
+
+            // Fill the histograms
+            _h[_hist_ZPt_ident]->fill(pT_Z);
+            _h[_hist_PhiStarEta_ident]->fill(phistareta); 
+
+            // End the loop, when a matching bin has been found
+            goto theEnd;
+          }
+          else continue;
+        }
+      }
+      theEnd:;
 
     }
 
@@ -94,9 +197,13 @@ namespace Rivet {
     /// Normalise histograms etc., after the run
     void finalize() {
 
-      normalize(_h["XXXX"]); // normalize to unity
-      normalize(_h["YYYY"], crossSection()/picobarn); // normalize to generated cross-section in fb (no cuts)
-      scale(_h["ZZZZ"], crossSection()/picobarn/sumW()); // norm to generated cross-section in pb (after cuts)
+      /// Normalise histograms
+      //const double sf = crossSection()/picobarn/sumW();
+      const double sf = 1.0;
+      
+      for(auto const& _hist : _h){
+        normalize(_hist.second, sf);
+      }
 
     }
 
@@ -105,9 +212,7 @@ namespace Rivet {
 
     /// @name Histograms
     ///@{
-    map<string, Histo1DPtr> _h;
-    map<string, Profile1DPtr> _p;
-    map<string, CounterPtr> _c;
+    map<string,Histo1DPtr> _h;
     ///@}
 
 
--- Law-Setup/generation/analyses/ZplusJet_3.plot
+++ Law-Setup/generation/analyses/ZplusJet_3.plot
@@ -1,8 +1,29 @@
-BEGIN PLOT /ZplusJet_3/d01-x01-y01
-Title=[Insert title for histogram d01-x01-y01 here]
-XLabel=[Insert $x$-axis label for histogram d01-x01-y01 here]
-YLabel=[Insert $y$-axis label for histogram d01-x01-y01 here]
-# + any additional plot settings you might like, see make-plots documentation
-END PLOT
+# BEGIN PLOT /ZplusJet/*
+LogX=1
+RatioPlotYMin=0.8
+RatioPlotYMax=1.2
+RatioPlotYLabel=NP corr.
+NormalizeToIntegral=1
+# END PLOT
+
+# BEGIN HISTOGRAM /ZplusJet/*
+ErrorBars=1
+# END HISTOGRAM
+
+# BEGIN HISTOGRAM /ZplusJet/Phi*
+XLabel=$\phi^*_{\eta}$
+YLabel=$\frac{1}{N}\frac{dN}{d\phi^*_{\eta} dy^* dy_b}$
+# END HISTOGRAM
+
+# BEGIN HISTOGRAM /ZplusJet/ZPt*
+XLabel=$p_{T_Z}$/GeV
+YLabel=$\frac{1}{N}\frac{dN}{dp_{T_Z} dy^* dy_b}$/$\frac{1}{\text{GeV}}$
+# END HISTOGRAM
+
+# BEGIN PLOT /ZplusJet/NJet*
+LogX=0
+XLabel=$N_{\text{jets}}$
+YLabel=$\frac{1}{N}\frac{dN}{dN_{\text{jets}} dy^* dy_b}$
+# END PLOT
 
 # ... add more histograms as you need them ...
