00ab5f35bdf30a260eb14968bda4fe74292b7100
Maximilian Maria Horzela <maximilian.horzela@kit.edu>
2020-11-25 16:39:19 +0100

    [Setup] More robust environment setting for Rivet

--- setup/setup_rivet.sh
+++ setup/setup_rivet.sh
@@ -1,16 +1,29 @@
 #!/bin/sh
 
-# source grid environment
-source /cvmfs/grid.cern.ch/centos7-wn-4.0.5-1_umd4v1/etc/profile.d/setup-c7-wn-example.sh
+action(){
+    # source grid environment
+    source /cvmfs/grid.cern.ch/centos7-wn-4.0.5-1_umd4v1/etc/profile.d/setup-c7-wn-example.sh
 
-# Set up Rivet
-# Set up GCC and Python (v3 also supported):
-source /cvmfs/sft.cern.ch/lcg/releases/LCG_96/Python/2.7.16/x86_64-centos7-gcc62-opt/Python-env.sh
-# Set up Rivet
-source /cvmfs/sft.cern.ch/lcg/releases/LCG_96b/MCGenerators/rivet/3.1.2/x86_64-centos7-gcc8-opt/rivetenv-genser.sh
-# Set up a working LaTeX (needed if on lxplus7)
-export PATH=/cvmfs/sft.cern.ch/lcg/external/texlive/2016/bin/x86_64-linux:$PATH
+    # Set up Rivet
+    # Set up GCC and Python (v3 also supported):
+    source /cvmfs/sft.cern.ch/lcg/releases/LCG_96/Python/2.7.16/x86_64-centos7-gcc62-opt/Python-env.sh
+    # Set up Rivet
+    source /cvmfs/sft.cern.ch/lcg/releases/LCG_96b/MCGenerators/rivet/3.1.2/x86_64-centos7-gcc8-opt/rivetenv-genser.sh
+    # Set up a working LaTeX (needed if on lxplus7)
+    export PATH=/cvmfs/sft.cern.ch/lcg/external/texlive/2016/bin/x86_64-linux:$PATH
 
-# Export rivet analysis path for plugin analyses
-export RIVET_ANALYSIS_PATH="$ANALYSIS_PATH/generation/analyses:$RIVET_ANALYSIS_PATH"
-#export RIVET_INFO_PATH="$RIVET_ANALYSIS_PATH"
+    # Export rivet analysis path for plugin analyses
+    if [ ! -z "$ZSH_VERSION" ]; then
+        local this_file="${(%):-%x}"
+    else
+        local this_file="${BASH_SOURCE[0]}"
+    fi
+    local base="$( cd "$( dirname "$this_file" )" && pwd)"
+    export ANALYSIS_PATH="$base/../Law-Setup"
+    export RIVET_ANALYSIS_PATH="$ANALYSIS_PATH/generation/analyses:$RIVET_ANALYSIS_PATH"
+    #export RIVET_INFO_PATH="$RIVET_ANALYSIS_PATH"
+}
+
+action "$@"
+
+    
