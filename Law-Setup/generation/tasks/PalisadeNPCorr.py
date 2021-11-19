bb9f0c9fbdf3fa67172c2ebc858afb4345696045
Maximilian Maria Horzela <maximilian.horzela@kit.edu>
2021-11-19 13:08:11 +0100

    reformatting

--- Law-Setup/generation/tasks/PalisadeNPCorr.py
+++ Law-Setup/generation/tasks/PalisadeNPCorr.py
@@ -10,7 +10,8 @@ from generation.framework import Task
 
 from ConvertYodaToRoot import ConvertYodaToRoot
 
-class PalisadeNPCorr(Task,law.LocalWorkflow):
+
+class PalisadeNPCorr(Task, law.LocalWorkflow):
     """
     plotting class for NP-correction factor plots with Palisade
     """
@@ -20,17 +21,16 @@ class PalisadeNPCorr(Task,law.LocalWorkflow):
     mc_setting_full = luigi.Parameter(default="withNP")
     mc_setting_NPoff = luigi.Parameter(default="NPoff")
 
-    mc_setting_pairs = [mc_setting_full,mc_setting_NPoff]
+    mc_setting_pairs = [mc_setting_full, mc_setting_NPoff]
 
     exclude_params_req = {
         "bootstrap_file"
     }
 
-
     def convert_env_to_dict(self, env):
         my_env = {}
         for line in env.splitlines():
-            if line.find(" ") < 0 :
+            if line.find(" ") < 0:
                 try:
                     key, value = line.split("=", 1)
                     my_env[key] = value
@@ -38,32 +38,34 @@ class PalisadeNPCorr(Task,law.LocalWorkflow):
                     pass
         return my_env
 
-
     def set_environment_variables(self):
-        code, out, error = interruptable_popen("source {}; env".format(os.path.join(os.path.dirname(__file__),"..","..","..","setup","setup_rivet.sh")),
-                                               shell=True, 
-                                               stdout=PIPE, 
-                                               stderr=PIPE
-                                               )
+        code, out, error = interruptable_popen(
+            "source {}; env".format(
+                os.path.join(
+                    os.path.dirname(__file__),
+                    "..", "..", "..",
+                    "setup",
+                    "setup_karma.sh"
+                    )
+                ),
+            shell=True,
+            stdout=PIPE,
+            stderr=PIPE
+        )
         my_env = self.convert_env_to_dict(out)
         return my_env
 
-    
     def create_branch_map(self):
         pass
-
 
     def workflow_requires(self):
         pass
 
-
     def requires(self):
         pass
-
 
     def output(self):
         pass
 
-    
     def run(self):
         pass
