c732ac7251d23fbae14593ec0d37706144ee89ab
Maximilian Maria Horzela <maximilian.horzela@kit.edu>
2022-01-21 11:10:12 +0100

    hotfix: rivet bootstrap

--- Law-Setup/generation/setup_lawrivet.sh
+++ Law-Setup/generation/setup_lawrivet.sh
@@ -6,6 +6,16 @@ action(){
     # source grid environment
     source /cvmfs/grid.cern.ch/centos7-wn-4.0.5-1_umd4v1/etc/profile.d/setup-c7-wn-example.sh
 
+
+    # Set up Rivet
+    source /cvmfs/sft.cern.ch/lcg/releases/LCG_96b/MCGenerators/rivet/3.1.2/x86_64-centos7-gcc8-opt/rivetenv-genser.sh
+    # Set up a working LaTeX (needed if on lxplus7)
+    export PATH=/cvmfs/sft.cern.ch/lcg/external/texlive/2016/bin/x86_64-linux:$PATH
+
+    
+    # Set up GCC and Python (v3 also supported):
+    source /cvmfs/sft.cern.ch/lcg/releases/LCG_96/Python/2.7.16/x86_64-centos7-gcc62-opt/Python-env.sh
+
     # untar tarball
     tar -xzf generation*.tar.gz
     rm generation*.tar.gz
@@ -21,13 +31,7 @@ action(){
     export PATH="$PWD/law/bin:$PWD/luigi/bin:$PATH"
     export PYTHONPATH="$PWD/enum34-1.1.10:$PWD/law:$PWD/luigi:$PWD/six:$PWD:$PYTHONPATH"
 
-    # Set up Rivet
-    # Set up GCC and Python (v3 also supported):
-    source /cvmfs/sft.cern.ch/lcg/releases/LCG_96/Python/2.7.16/x86_64-centos7-gcc62-opt/Python-env.sh
-    # Set up Rivet
-    source /cvmfs/sft.cern.ch/lcg/releases/LCG_96b/MCGenerators/rivet/3.1.2/x86_64-centos7-gcc8-opt/rivetenv-genser.sh
-    # Set up a working LaTeX (needed if on lxplus7)
-    export PATH=/cvmfs/sft.cern.ch/lcg/external/texlive/2016/bin/x86_64-linux:$PATH
+
     # Export rivet analysis path for plugin analyses
     export RIVET_ANALYSIS_PATH="$ANALYSIS_PATH/generation/analyses:$RIVET_ANALYSIS_PATH"
 
