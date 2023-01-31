23652a2bb040e52228f2b5b5aadff7147d1e1bb6
Maximilian Maria Horzela <maximilian.horzela@kit.edu>
2023-01-31 11:27:45 +0100

    fix padding

--- scripts/yodaPlotNPCorr.py
+++ scripts/yodaPlotNPCorr.py
@@ -259,7 +259,7 @@ for name, ao in aos_ratios.items():
 
     name = name.replace("/","_").strip("_")
 
-    fig.savefig(os.path.join(os.getcwd(), args.PLOTDIR, "{}.png".format(name)))
+    fig.savefig(os.path.join(os.getcwd(), args.PLOTDIR, "{}.png".format(name)), bbox_inches="tight")
 
     # yoda.plot(
     #     ao,
