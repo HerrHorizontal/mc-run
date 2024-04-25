17fc60db21a73e534b1212c86a4d1df32ca76461
Cedric Verstege <cedric.verstege@kit.edu>
2024-04-25 17:32:02 +0200

    Update sherpa source script

--- setup/setup_sherpa.sh
+++ setup/setup_sherpa.sh
@@ -9,7 +9,13 @@ source_sherpa() {
     this_dir="$( cd "$( dirname "$this_file" )" && pwd )"
 
     source /cvmfs/cms.cern.ch/cmsset_default.sh
-    pushd "$this_dir/../software/CMSSW_10_6_40/src" > /dev/null || exit
+    CMSSW_VERSION="CMSSW_10_6_40"
+    CMSSW_DIR="$this_dir/../software/$CMSSW_VERSION"
+    if [[ ! -d $CMSSW_DIR ]]; then
+        echo "Setting up Sherpa with CMSSW at $CMSSW_DIR"
+        scram project -d "$CMSSW_DIR/../" CMSSW "$CMSSW_VERSION"
+    fi
+    pushd "$CMSSW_DIR/src" > /dev/null || exit
     eval "$(scramv1 runtime -sh)"
     popd > /dev/null || exit
     export PATH=$PATH:/cvmfs/cms.cern.ch/slc7_amd64_gcc700/external/sherpa/2.2.15-3ed6122ae1412ab3c132a3b5c3c9d9ff/bin
