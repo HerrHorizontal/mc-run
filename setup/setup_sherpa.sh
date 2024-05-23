237afbc843a00e5ba08c64393f58bbd0ef210573
Cedric Verstege <cedric.verstege@kit.edu>
2024-05-23 17:14:40 +0200

    Update Sherpa Source script to run on multiple environments

--- setup/setup_sherpa.sh
+++ setup/setup_sherpa.sh
@@ -8,17 +8,33 @@ source_sherpa() {
     this_file="$( [ -n "$ZSH_VERSION" ] && echo "${(%):-%x}" || echo "${BASH_SOURCE[0]}" )"
     this_dir="$( cd "$( dirname "$this_file" )" && pwd )"
 
+    source "$this_dir/os-version.sh"
     source /cvmfs/cms.cern.ch/cmsset_default.sh
-    CMSSW_VERSION="CMSSW_10_6_40"
+
+    if [[ "$distro" == "CentOS" ]]; then
+        if [[ ${os_version:0:1} == "7" ]]; then
+            CMSSW_VERSION="CMSSW_12_4_20"
+        fi
+    elif [[ "$distro" == "RedHatEnterprise" || "$distro" == "Alma" || "$distro" == "Rocky" ]]; then
+        if [[ ${os_version:0:1} == "8" || ${os_version:0:1} == "9" ]]; then
+            CMSSW_VERSION="CMSSW_13_3_3"
+        fi
+    fi
+    if [[ -z "$CMSSW_VERSION" ]]; then
+        echo "CMSSW including Sherpa not available for $distro $os_version"
+        return 1
+    fi
     CMSSW_DIR="$this_dir/../software/$CMSSW_VERSION"
     if [[ ! -d $CMSSW_DIR ]]; then
         echo "Setting up Sherpa with CMSSW at $CMSSW_DIR"
         scram project -d "$CMSSW_DIR/../" CMSSW "$CMSSW_VERSION"
     fi
     pushd "$CMSSW_DIR/src" > /dev/null || exit
+    echo "Loading Sherpa with $CMSSW_VERSION"
     eval "$(scramv1 runtime -sh)"
+    sherpa_path="$(scram tool info Sherpa | grep BINDIR | cut -d "=" -f2)"
+    export PATH="$PATH:$sherpa_path"
     popd > /dev/null || exit
-    export PATH=$PATH:/cvmfs/cms.cern.ch/slc7_amd64_gcc700/external/sherpa/2.2.15-3ed6122ae1412ab3c132a3b5c3c9d9ff/bin
     Sherpa --version
 }
 
