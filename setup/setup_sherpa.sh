0fe9c4109f0c1ccf57c3bd6fdec1e51c19c1a4cb
Cedric Verstege <cedric.verstege@kit.edu>
2024-05-24 16:48:05 +0200

    Sherpa Setup: Source LCG stack as base env

--- setup/setup_sherpa.sh
+++ setup/setup_sherpa.sh
@@ -11,6 +11,7 @@ this_dir="$( cd "$( dirname "$this_file" )" && pwd )"
 source_sherpa() {
     # Check OS and source Sherpa with dependencies from according LCG Stack
     base=/cvmfs/sft.cern.ch/lcg/releases
+    view_base=/cvmfs/sft.cern.ch/lcg/views
     LCG=LCG_105
     local prefix
     source "$this_dir/os-version.sh"
@@ -32,34 +33,28 @@ source_sherpa() {
         return 1
     fi
     platform=${prefix}-gcc11-opt
-    gccver=11.3.0-ad0f5
+    local lcg_path=$view_base/$LCG/$platform/setup.sh
+    echo "Sourcing LCG Stack from $lcg_path"
+    # shellcheck disable=SC1090
+    source "$lcg_path"
 
-    export LD_LIBRARY_PATH=${base}/gcc/${gccver}/${prefix}/lib:$LD_LIBRARY_PATH
-    export LD_LIBRARY_PATH=${base}/gcc/${gccver}/${prefix}/lib64:$LD_LIBRARY_PATH
-    export LD_LIBRARY_PATH=${base}/binutils/2.37-355ed/${prefix}/lib:$LD_LIBRARY_PATH
-    export LD_LIBRARY_PATH=${base}/${LCG}/CppUnit/1.14.0/${platform}/lib:$LD_LIBRARY_PATH
-    export LD_LIBRARY_PATH=${base}/${LCG}/Python/3.9.12/${platform}/lib:$LD_LIBRARY_PATH
-    export LD_LIBRARY_PATH=${base}/${LCG}/sqlite/3320300/${platform}/lib:$LD_LIBRARY_PATH
+    # Add MC Generators to LD_LIBRARY_PATH
     export LD_LIBRARY_PATH=${base}/${LCG}/fastjet/3.4.1/${platform}/lib:$LD_LIBRARY_PATH
     export LD_LIBRARY_PATH=${base}/${LCG}/MCGenerators/openloops/2.1.2/${platform}/lib:$LD_LIBRARY_PATH
     export LD_LIBRARY_PATH=${base}/${LCG}/MCGenerators/openloops/2.1.2/${platform}/proclib:$LD_LIBRARY_PATH
     export LD_LIBRARY_PATH=${base}/${LCG}/MCGenerators/lhapdf/6.5.3/${platform}/lib:$LD_LIBRARY_PATH
     export LD_LIBRARY_PATH=${base}/${LCG}/MCGenerators/sherpa/2.2.15/${platform}/lib/SHERPA-MC:$LD_LIBRARY_PATH
+    # For some reason hepmc3 has separate lib and lib64 directories depending on the OS
     if [[ "$distro" == "Ubuntu" ]]; then
         export LD_LIBRARY_PATH=${base}/${LCG}/hepmc3/3.2.7/${platform}/lib:$LD_LIBRARY_PATH
     else
         export LD_LIBRARY_PATH=${base}/${LCG}/hepmc3/3.2.7/${platform}/lib64:$LD_LIBRARY_PATH
     fi
-
-    export PATH=${base}/gcc/${gccver}/${prefix}/bin:$PATH
-    export PATH=${base}/binutils/2.37-355ed/${prefix}/bin:$PATH
-    export PATH=${base}/${LCG}/numpy/1.23.5/${platform}/bin:$PATH
-    export PATH=${base}/${LCG}/sqlite/3320300/${platform}/bin:$PATH
-    export PATH=${base}/${LCG}/Python/3.9.12/${platform}/bin:$PATH
-    export PATH=${base}/${LCG}/pip/22.0.4/${platform}/bin::$PATH
+    # Add Sherpa to Path
     export PATH=${base}/${LCG}/MCGenerators/sherpa/2.2.15/${platform}/bin:$PATH
-
+    # Specify LHAPDF path and the OpenLoops prefix
     export LHAPDF_DATA_PATH=$LHAPDF_DATA_PATH:${base}/${LCG}/MCGenerators/lhapdf/6.5.3/${platform}/share/LHAPDF:/cvmfs/sft.cern.ch/lcg/external/lhapdfsets/current
+    export OL_PREFIX=${base}/${LCG}/MCGenerators/openloops/2.1.2/${platform}/
 }
 
 source_sherpa "$@"
