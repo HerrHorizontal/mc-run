848efcb53c18f38c6999887264ae7fab744c804a
Cedric Verstege <cedric.verstege@kit.edu>
2024-05-24 18:08:07 +0200

    Update herwig setup script, source herwig from lcg 105

--- setup/setup_herwig.sh
+++ setup/setup_herwig.sh
@@ -1,16 +1,66 @@
-#!/bin/sh
+#!/bin/bash
 
-# source grid environment
-source /cvmfs/grid.cern.ch/centos7-wn-4.0.5-1_umd4v1/etc/profile.d/setup-c7-wn-example.sh
+# determine the directy of this file
+if [ -n "$ZSH_VERSION" ]; then
+    this_file="${(%):-%x}"
+else
+    this_file="${BASH_SOURCE[0]}"
+fi
+this_dir="$( cd "$( dirname "$this_file" )" && pwd )"
 
-# setup Herwig
-export INSTALL_LOC=/cvmfs/etp.kit.edu/herwig/
-export HERWIGPATH=/cvmfs/etp.kit.edu/herwig/
-export PATH=$INSTALL_LOC/bin:$PATH
-export LHAPDF=/cvmfs/etp.kit.edu/herwig/
-export PATH=$PATH:$LHAPDF/bin
-export LD_LIBRARY_PATH=$LHAPDF/lib:$LD_LIBRARY_PATH
-export PYTHONPATH=$LHAPDF/lib/python2.7/site-packages:$PYTHONPATH
-# activate Herwig
-source /cvmfs/etp.kit.edu/herwig/bin/activate
+source_herwig() {
+    # Check OS and source Sherpa with dependencies from according LCG Stack
+    base=/cvmfs/sft.cern.ch/lcg/releases
+    view_base=/cvmfs/sft.cern.ch/lcg/views
+    LCG=LCG_105
+    local prefix
+    source "$this_dir/os-version.sh"
+    if [[ "$distro" == "CentOS" ]]; then
+        if [[ ${os_version:0:1} == "7" ]]; then
+            prefix=x86_64-centos7
+        fi
+    elif [[ "$distro" == "RedHatEnterprise" || "$distro" == "Alma" || "$distro" == "Rocky" ]]; then
+        if [[ ${os_version:0:1} == "9" ]]; then
+            prefix=x86_64-el9
+        fi
+    elif [[ "$distro" == "Ubuntu" ]]; then
+        if [[ ${os_version:0:2} == "22" ]]; then
+            prefix=x86_64-ubuntu2204
+        fi
+    fi
+    if [[ -z "$prefix" ]]; then
+        echo "Sherpa with LCG Stack $LCG not available for $distro $os_version"
+        return 1
+    fi
+    platform=${prefix}-gcc11-opt
+    local lcg_path=$view_base/$LCG/$platform/setup.sh
+    echo "Sourcing LCG Stack from $lcg_path"
+    # shellcheck disable=SC1090
+    source "$lcg_path"
 
+    # Add MC Generators to LD_LIBRARY_PATH
+    export LD_LIBRARY_PATH=${base}/${LCG}/fastjet/3.4.1/${platform}/lib:$LD_LIBRARY_PATH
+    export LD_LIBRARY_PATH=${base}/${LCG}/MCGenerators/openloops/2.1.2/${platform}/lib:$LD_LIBRARY_PATH
+    export LD_LIBRARY_PATH=${base}/${LCG}/MCGenerators/openloops/2.1.2/${platform}/proclib:$LD_LIBRARY_PATH
+    export LD_LIBRARY_PATH=${base}/${LCG}/MCGenerators/lhapdf/6.5.3/${platform}/lib:$LD_LIBRARY_PATH
+    export LD_LIBRARY_PATH=${base}/${LCG}/MCGenerators/herwig++/7.2.3/${platform}/lib/Herwig:$LD_LIBRARY_PATH
+    # For some reason hepmc3 has separate lib and lib64 directories depending on the OS
+    if [[ "$distro" == "Ubuntu" ]]; then
+        export LD_LIBRARY_PATH=${base}/${LCG}/hepmc3/3.2.7/${platform}/lib:$LD_LIBRARY_PATH
+    else
+        export LD_LIBRARY_PATH=${base}/${LCG}/hepmc3/3.2.7/${platform}/lib64:$LD_LIBRARY_PATH
+    fi
+    # Add Herwig to Path
+    export PATH=${base}/${LCG}/MCGenerators/herwig++/7.2.3/${platform}/bin:$PATH
+    export HERWIGPATH=${base}/${LCG}/MCGenerators/herwig++/7.2.3/${platform}/share/Herwig
+    export PYTHONPATH=${base}/${LCG}/MCGenerators/herwig++/7.2.3/${platform}/lib/Herwig/python:$PYTHONPATH
+    # Specify LHAPDF path and the OpenLoops prefix
+    export LHAPDF_DATA_PATH=$LHAPDF_DATA_PATH:${base}/${LCG}/MCGenerators/lhapdf/6.5.3/${platform}/share/LHAPDF:/cvmfs/sft.cern.ch/lcg/external/lhapdfsets/current
+    export OL_PREFIX=${base}/${LCG}/MCGenerators/openloops/2.1.2/${platform}/
+
+    # Point Herwig to it's installation directory
+    alias Herwig='Herwig --repo $HERWIGPATH/HerwigDefaults.rpo'
+}
+
+source_herwig "$@"
+Herwig --version
