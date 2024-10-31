61894ab52064c1451249985b5fb2bd00c02e7b13
Cedric Verstege <cedric.verstege@cern.ch>
2024-10-31 16:14:37 +0100

    Update HTCondor.py and settings

--- generation/framework/htcondor/HTCondor.py
+++ generation/framework/htcondor/HTCondor.py
@@ -1,5 +1,6 @@
 import os
 import socket
+import math
 from enum import Enum
 
 import law
@@ -32,9 +33,11 @@ class HTCondorWorkflow(law.htcondor.HTCondorWorkflow):
         significant=False,
         description="ETP HTCondor specific flag to allow jobs to run on remote resources.",
     )
-    htcondor_walltime = luigi.Parameter(
+    htcondor_walltime = law.DurationParameter(
+        default=1.0,
+        unit="h",
         significant=False,
-        description="Requested walltime for the jobs.",
+        description="Maximum runtime for jobs. Default unit is hours. Defaults to 1h.",
     )
     htcondor_request_cpus = luigi.Parameter(
         default=ConfigParser.get("HTCondorDefaults", "htcondor_request_cpus"),
@@ -87,58 +90,48 @@ class HTCondorWorkflow(law.htcondor.HTCondorWorkflow):
     create_branch_map_before_repr = True
 
     def htcondor_workflow_requires(self):
-        reqs = law.htcondor.HTCondorWorkflow.htcondor_workflow_requires(self)
-        # add repo and software bundling as requirements when getenv is not requested
+        reqs = super().htcondor_workflow_requires()
         reqs["repo"] = BundleRepo.req(self, replicas=3)
 
         return reqs
-
-    def htcondor_create_job_manager(self, **kwargs):
-        kwargs = merge_dicts(self.htcondor_job_manager_defaults, kwargs)
-        job_manager = super().htcondor_create_job_manager(**kwargs)
-        job_manager.job_grouping_submit = False
-        job_manager.chunk_size_submit = 0
-        return job_manager
 
     def htcondor_output_directory(self):
         job_dir = law.config.get_expanded("job", "job_file_dir")
         return law.LocalDirectoryTarget(f"{job_dir}/{self.task_id}/")
 
     def htcondor_create_job_file_factory(self):
-        factory = super(HTCondorWorkflow, self).htcondor_create_job_file_factory(
+        factory = super().htcondor_create_job_file_factory(
             dir=self.htcondor_output_directory().abspath
         )
-        factory.is_tmp = False
         return factory
 
     def htcondor_bootstrap_file(self):
-        return law.util.rel_path(__file__, self.bootstrap_file)
+        bootstrap_file = law.util.rel_path(__file__, self.bootstrap_file)
+        return law.JobInputFile(bootstrap_file, share=True, render_job=True)
 
     def htcondor_job_config(self, config, job_num, branches):
-        config.log = os.path.join("Log.txt")
-        config.stdout = os.path.join("Output.txt")
-        config.stderr = os.path.join("Error.txt")
+        config.universe = self.htcondor_universe
         config.custom_content = []
+        config.custom_content.append(("x509userproxy", law.wlcg.get_vomsproxy_file()))
+        config.custom_content.append(("stream_error", "True"))
+        config.custom_content.append(("stream_output", "True"))
+        config.custom_content.append(("docker_image", self.htcondor_docker_image))
+        # max runtime
+        max_runtime = int(math.floor(self.htcondor_walltime * 3600)) - 1
+        config.custom_content.append(("+MaxRuntime", max_runtime))
+        config.custom_content.append(("+RequestRuntime", max_runtime))
         if self.domain == self.Domain.ETP:
             config.custom_content.append(
                 ("accounting_group", self.htcondor_accounting_group)
             )
             config.custom_content.append(("+RemoteJob", self.htcondor_remote_job))
-        config.custom_content.append(("stream_error", "True"))
-        config.custom_content.append(("stream_output", "True"))
-        config.custom_content.append(("Requirements", self.htcondor_requirements))
-        config.custom_content.append(("universe", self.htcondor_universe))
-        config.custom_content.append(("docker_image", self.htcondor_docker_image))
-        if self.domain == self.Domain.CERN:
-            config.custom_content.append(("+MaxRuntime", self.htcondor_walltime))
-        elif self.domain == self.Domain.ETP:
-            config.custom_content.append(("+RequestWalltime", self.htcondor_walltime))
-        config.custom_content.append(("x509userproxy", law.wlcg.get_vomsproxy_file()))
+            config.custom_content.append(("+RequestWalltime", max_runtime))
         config.custom_content.append(("request_cpus", self.htcondor_request_cpus))
+        config.custom_content.append(("RequestCpus", self.htcondor_request_cpus))
         config.custom_content.append(("RequestMemory", self.htcondor_request_memory))
         config.custom_content.append(("RequestDisk", self.htcondor_request_disk))
-
         config.custom_content.append(("JobBatchName", self.task_id))
+        config.custom_content.append(("Requirements", self.htcondor_requirements))
 
         # include the wlcg specific tools script in the input sandbox
         tools_file = law.util.law_src_path("contrib/wlcg/scripts/law_wlcg_tools.sh")
